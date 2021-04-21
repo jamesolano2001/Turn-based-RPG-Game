40ac91540b104e123aeb50993ff54a8eccdc2355
James Olano <79013899+u3577797@users.noreply.github.com>
2021-04-22 00:14:17 +0800

    Update player.h
    Change from class to struct for simplicity

--- player.h
+++ player.h
@@ -1,22 +1,4 @@
 #include <string>
-
 using namespace std;
 
-class Player
-{
-public:                            // these are just the basic stats of the player, still thinking of how to implement the stage/inventory
-    void setName(string);
-    void health(double);
-    void maxHealth();
-    void setDamage();
-    string getName();
-    double getHealth();
-    double getMaxHealth();
-    int getDamage();
-    private:
-    string pName;
-    double pHealth;
-    double pMaxHealth;
-    int pDamage;
-    int pLevel;
-};
+struct Player;
