e6df5a3a0ebe38ac68328057b567352182e2d3c6
James Olano <79013899+u3577797@users.noreply.github.com>
2021-04-22 00:13:14 +0800

    Update player.cpp
    Change from using classes to struct for simplicity

--- player.cpp
+++ player.cpp
@@ -1,24 +1,15 @@
 #include <iostream>
 #include <string>
+#include <vector>
 #include "player.h"
 
 using namespace std;
 
-player::player(string name, int score = 0, int level = 1)
-{
-  setName(name);
-  setLevel(level);
-  setMaxHealth();
-  setHealth(pMaxHealth);
-  setDamage();
-}
-
-void player::setName(string name)
-{
-  pName = name;
-}
-
-void player::setLevel(int level)
-{
-  pLevel = level;
-}
+struct Player{
+    string name;
+    int health;
+    vector<string> inventory;
+    int score;
+    int stage;
+  
+};
