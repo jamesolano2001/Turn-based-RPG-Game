a45537860e8cedd2695c6d104d8b568234e2ed48
James Olano <79013899+u3577797@users.noreply.github.com>
2021-04-25 01:41:46 +0800

    Create acidville.cpp
    Alpha build of the town resting area

---
+++ acidville.cpp
@@ -0,0 +1,61 @@
+#include <iostream>
+#include <string>
+#include <cstdlib>
+#include <time.h>
+using namespace std;
+
+
+void town()
+{
+    cout<<"  ~         ~~          __         "<<endl;
+    cout<<"       _T      .,,.    ~--~ ^^      "<<endl;
+    cout<<" ^^   // \\                    ~      "<<endl;
+    cout<<"      ][O]    ^^      ,-~ ~          "<<endl;
+    cout<<"   /''-I_I         _II____           "<<endl;
+    cout<<"__/_  /   \\ ______/ ''   /'\\_,__     "<<endl;
+    cout<<"  | II--'''' \\,--:--..,_/,.-{ },     "<<endl;
+    cout<<"; '/__\\,.--';|   |[] .-.| O{ _ }     "<<endl;
+    cout<<":' |  | []  -|   ''--:.;[,.'\\,/      "<<endl;
+    cout<<"'  |[]|,.--'' '',   ''-,.    |       "<<endl;
+    cout<<"  ..    ..-''    ;       ''. '       "<<endl;
+}
+
+void menu()
+{
+	town();
+	cout<<"Welcome to Acid Ville"<<endl;
+	cout<<"---------------------"<<endl;
+	cout<<"1. Venture forth into thee forest"<<endl;
+	cout<<"2. Rest"<<endl;
+	cout<<"3. Save"<<endl;
+}
+
+void townone()
+{
+	int option = 0;
+	srand(time(NULL));
+	while ( option != 1  ){
+		menu();
+		int random = rand()%2;
+		cin >> option;
+		if ( option == 2 ){
+			clearscreen();
+			cout<<"zzzZZZ... zzzZZZZZzz... zzzZZZ....."<<endl;
+			cin.ignore();
+			clearscreen();
+			if ( random == 0){
+				cout<<"Health is now full, that was a good nap!"<<endl;
+				cin.ignore();
+			}
+			else{
+				cout<<"Ouch! The bed bugs have infested the house! What an awful nap!"<<endl;
+				cin.ignore();
+			}
+			option = 0;
+		}
+	}
+
+
+
+
+}
