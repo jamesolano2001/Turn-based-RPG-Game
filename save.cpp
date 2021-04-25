203ffae63d99d93a295822ea44cc216e0c93b143
3vl01 <78291071+3vl01@users.noreply.github.com>
2021-04-26 02:09:28 +0800

    Update save.cpp

--- save.cpp
+++ save.cpp
@@ -14,10 +14,16 @@ void save(string n, string i, int Score, int Stage, int order, int h, int mh){
     cout << "Fail saving" << endl;
     exit(1);
   }
-  fout << i <<endl;
+  for (int j=0; j<i.size()-1; j++){
+    fout << i.at(j) << "," ;
+  }
+  fout << i.at(i.size()) <<endl;
   fout << Score <<endl;
   fout << Stage <<endl;
-  fout << order <<endl;
+  for (int k=0; k<2; k++){
+    fout << order.at(k) << "," ;
+  }
+  fout << order.at(2) << endl;
   fout << h <<endl;
   fout << mh <<endl;
   fout.close();
@@ -25,6 +31,7 @@ void save(string n, string i, int Score, int Stage, int order, int h, int mh){
 
 int main(){
   player();
+  Player player;
   save(player.name, player.inventory, player.score, player.stage, player.order, player.health, player.maxHealth);
   return 0;
 }
