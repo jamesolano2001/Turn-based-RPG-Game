e43912cdca51a7c241347997c2d3ff17bd809cf7
3vl01 <78291071+3vl01@users.noreply.github.com>
2021-04-26 01:54:47 +0800

    Update load.cpp

--- load.cpp
+++ load.cpp
@@ -1,160 +1,150 @@
 #include <iostream>
 #include <string>
 #include <vector>
+#include <stdlib.h>
 #include <fstream>
-#include "player.h"
+#include <iterator>
+#include <sstream>
+#inlcude "player.h"
 
 using namespace std;
 
-string loadi(string n){
-  string filename=n+".txt"
-  string i;
-  ifstream fout;
-  fin.open(filename);
-  if (fin.fail()){
-    cout << "Fail loading" << endl;
-    exit(1);
-  }
-  string line;
-  int number_of_lines = 0;
-  ifstream myfile(filename);
-  while (getline(myfile, line)){
-    ++number_of_lines;
-  }
-  for (int j = 1; j <= (number_of_lines-6); j++){
-    getline(f, line);
-  }
-  i = line;
-  fin.close();
-  return i;
+vector<string> loadi(string n){
+    string filename=n+".txt";
+    vector<string> i;
+    ifstream fin;
+    fin.open(filename);
+    if (fin.fail()){
+        cout << "Fail loading" << endl;
+        exit(1);
+    }
+    string line;
+    ifstream myfile(filename);
+    for (int j = 0; j <= 0; j++){
+        getline(myfile, line);
+    }
+    string item;
+    stringstream line2;
+    line2.str(line);
+    while (getline(line2, item, ',')) {
+        i.push_back(item);
+    }
+    fin.close();
+    return i;
 }
 
 int loadsc(string n){
-  string filename=n+".txt"
-  int sc;
-  ifstream fout;
-  fin.open(filename);
-  if (fin.fail()){
-    cout << "Fail loading" << endl;
-    exit(1);
-  }
-  string line;
-  int number_of_lines = 0;
-  ifstream myfile(filename);
-  while (getline(myfile, line)){
-    ++number_of_lines;
-  }
-  for (int i = 1; i <= (number_of_lines-5); i++){
-    getline(f, line);
-  }
-  sc = atoi(line);
-  fin.close();
-  return sc;
+    string filename=n+".txt";
+    int sc;
+    ifstream fin;
+    fin.open(filename);
+    if (fin.fail()){
+      cout << "Fail loading" << endl;
+      exit(1);
+    }
+    string line;
+    ifstream myfile(filename);
+    for (int i = 0; i <= 1; i++){
+      getline(myfile, line);
+    }
+    sc = stoi(line);
+    fin.close();
+    return sc;
 }
 
 int loadst(string n){
-  string filename=n+".txt"
-  int st;
-  ifstream fout;
-  fin.open(filename);
-  if (fin.fail()){
-    cout << "Fail loading" << endl;
-    exit(1);
-  }
-  string line;
-  int number_of_lines = 0;
-  ifstream myfile(filename);
-  while (getline(myfile, line)){
-    ++number_of_lines;
-  }
-  for (int i = 1; i <= (number_of_lines-4); i++){
-    getline(f, line);
-  }
-  st = atoi(line);
-  fin.close();
-  return st;
+    string filename=n+".txt";
+    int st;
+    ifstream fin;
+    fin.open(filename);
+    if (fin.fail()){
+      cout << "Fail loading" << endl;
+      exit(1);
+    }
+    string line;
+    ifstream myfile(filename);
+    for (int i = 0; i <= 2; i++){
+      getline(myfile, line);
+    }
+    st = stoi(line);
+    fin.close();
+    return st;
 }
 
-int loado(string n){
-  string filename=n+".txt"
-  int o;
-  ifstream fout;
-  fin.open(filename);
-  if (fin.fail()){
-    cout << "Fail loading" << endl;
-    exit(1);
-  }
-  string line;
-  int number_of_lines = 0;
-  ifstream myfile(filename);
-  while (getline(myfile, line)){
-    ++number_of_lines;
-  }
-  for (int i = 1; i <= (number_of_lines-3); i++){
-    getline(f, line);
-  }
-  o = atoi(line);
-  fin.close();
-  return o;
+vector<int> loado(string n){
+    string filename=n+".txt";
+    vector<int> o;
+    ifstream fin;
+    fin.open(filename);
+    if (fin.fail()){
+      cout << "Fail loading" << endl;
+      exit(1);
+    }
+    string line;
+    ifstream myfile(filename);
+    for (int i = 0; i <= 3; i++){
+      getline(myfile, line);
+    }
+    string item;
+    stringstream line2;
+    line2.str(line);
+    while (getline(line2, item, ',')) {
+        o.push_back(stoi(item));
+    }
+    fin.close();
+    return o;
 }
 
 int loadh(string n){
-  string filename=n+".txt"
-  int h;
-  ifstream fout;
-  fin.open(filename);
-  if (fin.fail()){
-    cout << "Fail loading" << endl;
-    exit(1);
-  }
-  string line;
-  int number_of_lines = 0;
-  ifstream myfile(filename);
-  while (getline(myfile, line)){
-    ++number_of_lines;
-  }
-  for (int i = 1; i <= (number_of_lines-2); i++){
-    getline(f, line);
-  }
-  h = atoi(line);
-  fin.close();
-  return h;
+    string filename=n+".txt";
+    int h;
+    ifstream fin;
+    fin.open(filename);
+    if (fin.fail()){
+      cout << "Fail loading" << endl;
+      exit(1);
+    }
+    string line;
+    ifstream myfile(filename);
+    for (int i = 0; i <= 4; i++){
+      getline(myfile, line);
+    }
+    h = stoi(line);
+    fin.close();
+    return h;
 }
 
 int loadmh(string n){
-  string filename=n+".txt"
-  int mh;
-  ifstream fout;
-  fin.open(filename);
-  if (fin.fail()){
-    cout << "Fail loading" << endl;
-    exit(1);
-  }
-  string line;
-  int number_of_lines = 0;
-  ifstream myfile(filename);
-  while (getline(myfile, line)){
-    ++number_of_lines;
-  }
-  for (int i = 1; i <= (number_of_lines-1); i++){
-    getline(f, line);
-  }
-  mh = atoi(line);
-  fin.close();
-  return mh;
+    string filename=n+".txt";
+    int mh;
+    ifstream fin;
+    fin.open(filename);
+    if (fin.fail()){
+      cout << "Fail loading" << endl;
+      exit(1);
+    }
+    string line;
+    ifstream myfile(filename);
+    for (int i = 0; i <= 5; i++){
+      getline(myfile, line);
+    }
+    mh = stoi(line);
+    fin.close();
+    remove(filename.c_str());
+    return mh;
 }
 
 int main(){
-  player();
-  Player a;
-  cout << "Welcome back, my friend:)" << endl;
-  cout << "Please tell us your name" <<endl;
-  cin >> a;
-  player.inventory = loadi(player.name);
-  player.score = loadsc(player.name);
-  player.stage = loadst(player.name);
-  player.order = loado(player.name);
-  player.health = loadh(player.name);
-  player.maxHealth = loadmh(player.name);
-  return 0;
+    player();
+    Player a;
+    cout << "Welcome back, my friend:)" << endl;
+    cout << "Please tell us your name" <<endl;
+    cin >> a.name;
+    a.inventory = loadi(a.name);
+    a.score = loadsc(a.name);
+    a.stage = loadst(a.name);
+    a.order = loado(a.name);
+    a.health = loadh(a.name);
+    a.maxHealth = loadmh(a.name);
+    return 0;
 }
