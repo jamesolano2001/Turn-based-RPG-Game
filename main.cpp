5464bfafcfee0abdf8bc5c8a3b6892ac7175b4a4
James Olano <79013899+u3577797@users.noreply.github.com>
2021-04-26 01:14:49 +0800

    Update main.cpp
    Revamped structure of game flow ( Before the beginning function was inside titlescreen and the acidville function was inside beginning ) Which is like a chain of files. I wanted the main file to have direct access to most of the files.

--- main.cpp
+++ main.cpp
@@ -1,11 +1,54 @@
 #include <iostream>
 #include <string>
 #include "titlescreen.h"
-
+#include "beginning.h"
+#include "player.h"
+#include "acidville.h"
 
 using namespace std;
 
+
+void randomgenerate()
+{
+	const int AMOUNT = 3;
+
+
+	int townpath[AMOUNT];
+
+	srand(time(NULL));
+
+	for (int i=0;i<AMOUNT;i++)
+	{
+		bool unique;
+		int n;
+		do
+		{
+
+		n=rand()%AMOUNT;
+		unique=true;
+		for (int j=0;j<i;j++)
+			if (n == townpath[j])
+			{
+				unique = false;
+				break;
+			}
+		} while (!unique);
+		townpath[i]=n;
+	}
+	for ( int i = 0; i < AMOUNT; i++){
+		player.order[i] = townpath[i];
+	}
+
+
+
+}
+
 int main()
 {
+	struct Player player;
+	randomgenerate();
     titlescreen();
+	beginning();
+	acidville();
+
 }
