dc11165362710c2da353d3745676060d70298ddc
3vl01 <78291071+3vl01@users.noreply.github.com>
2021-04-26 20:34:27 +0800

    Update save.cpp

--- save.cpp
+++ save.cpp
@@ -15,7 +15,6 @@ void save(){
         cout << "Fail saving" << endl;
         exit(1);
     }
-    fout << player.inventory[0] << "," << player.inventory[1] << "," << player.inventory[2] <<endl;
     fout << player.score <<endl;
     fout << player.stage <<endl;
     fout << player.order[0] << "," << player.order[1] << "," << player.order[2] <<endl;
