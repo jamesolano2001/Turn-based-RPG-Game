64f40779425d40a248fdca3af3a611d3e88a08ad
3vl01 <78291071+3vl01@users.noreply.github.com>
2021-04-26 21:47:53 +0800

    Update save.cpp

--- save.cpp
+++ save.cpp
@@ -8,6 +8,7 @@
 using namespace std;
 
 void save(){
+    //create a file with the username
     string filename=player.name+".txt";
     ofstream fout;
     fout.open(filename);
@@ -15,11 +16,13 @@ void save(){
         cout << "Fail saving" << endl;
         exit(1);
     }
+    //writing different information in separate lines, so that the info could be easily obtain in the load function
     fout << player.score <<endl;
     fout << player.stage <<endl;
     fout << player.order[0] << "," << player.order[1] << "," << player.order[2] <<endl;
     fout << player.health <<endl;
     fout << player.maxHealth <<endl;
     fout.close();
+    //return to the very beginning menu of the game
     titlescreen();
 }
