d6677d8188067440fd8aee1e4dc8b5e6e0b6d560
3vl01 <78291071+3vl01@users.noreply.github.com>
2021-04-26 21:29:35 +0800

    Update acid.cpp

--- acid.cpp
+++ acid.cpp
@@ -12,19 +12,25 @@ void acid_battle()
     int players_healthbar=player.health;
     int monsters_healthbar=100;
     cout<<"There is a pond in front of your house, but it was polluted by your hair dyes which contains ammonia. The fishes living in the pond evoluted to creatures that eat humans. However, you realised that they could only survive in an alkaline environment after your observation. There are 4 bottles on the shelf in your storeroom. Which would you choose to pour into the pond?"<<endl;
+    //print out the question
     cout << "Your health bar: " << players_healthbar <<endl;
     cout << "Monster's health bar: " << monsters_healthbar <<endl;
+    //print out the health bars
     cout << "( |><_>   <_><|  |><_> )"<< endl;
     cout << "A) Coke" << endl;
     cout << "B) Toilet bowl cleaner" << endl;
     cout << "C) Window cleaner" << endl;
     cout << "D) Milk" << endl;
     cout << "Q) Quit" << endl;
+    //print out the choices and the player can choose to quit anytime
+    //if the player or the monster's health bar reached 0, the battle ends
     while(players_healthbar>0 && monsters_healthbar>0){
+	//shows the health bars everytime he/she makes a choice to tell if he/she is correct
         cout << "Your health bar: " << players_healthbar <<endl;
         cout << "Monster's health bar: " << monsters_healthbar <<endl;
         cin >> choice;
         switch(choice){
+	    //responding to his/her answers
             case 'A':
                 monsters_healthbar-=20;
                 players_healthbar-=25;
@@ -47,11 +53,13 @@ void acid_battle()
                 char ans;
                 cin >> ans;
                 if(ans=='y'){
+		    //if the player chose to quit, he/she will be directed to the village
                     cout<<"You have been revived back to your recent checkpoint"<<endl;
 		    cin.ignore();
 		    acidville();
                 }
                 else{
+		    //if he/she typed n or other stuff, the battle will continue
                     cout<< "Invalid option" << endl;
                     cin >> choice;
                 }
@@ -60,13 +68,15 @@ void acid_battle()
                 cin >> choice;
         }
     }
-	cin.ignore();
+    cin.ignore();
+    //gives out the correct answer with explaination as this is an educational game
     cout << "B is the correct answer as toilet bowl cleaners are acidic enough to neutralize the pond." << endl;
-	cin.ignore();
+    cin.ignore();
     if(players_healthbar>monsters_healthbar && players_healthbar!=0){
-		clearscreen();
+	clearscreen();
         cout << "You win!!!" <<endl;
 		cin.ignore();
+	    	//the health bar will be added to the player.score only when he/she wins in this battle
 		player.score += players_healthbar;
 		cout << "Your score is " << players_healthbar <<endl;
 	   	cin.ignore();
@@ -74,9 +84,11 @@ void acid_battle()
     else{
 	clearscreen();
         cout << "You lose..." <<endl;
+	    	//go back to the village to replay this level
 		cout<<"You have been revived back to your recent checkpoint"<<endl;
 		cin.ignore();
 		acidville();
     }
+    //move forward to the next checkpoint
     return;
 }
