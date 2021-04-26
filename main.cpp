73c1169c0d491278bd0037f27a2052ecbcf95574
James Olano <79013899+u3577797@users.noreply.github.com>
2021-04-26 20:31:44 +0800

    Update main.cpp
    Used pointer for allocating random town path

--- main.cpp
+++ main.cpp
@@ -18,6 +18,8 @@ void randomgenerate()
 
 
 	int townpath[AMOUNT];
+	int *ptr;				//we used a pointer to allocate the random integers to the array.
+	ptr = townpath;
 
 	srand(time(NULL));
 
@@ -31,16 +33,16 @@ void randomgenerate()
 		n=rand()%AMOUNT;
 		unique=true;
 		for (int j=0;j<i;j++)
-			if (n == townpath[j])
+			if (n == ptr[j])
 			{
 				unique = false;
 				break;
 			}
 		} while (!unique);
-		townpath[i]=n;
+		ptr[i]=n;
 	}
 	for ( int i = 0; i < AMOUNT; i++){
-		player.order.push_back(townpath[i]);
+		player.order.push_back(ptr[i]);
 	}
 
 }
@@ -49,6 +51,7 @@ void randomgenerate()
 void townpath()
 {
 	int i = 0;
+	
 	while (i < 3){
 		player.stage = i;
 		switch (player.order[i]+1){
