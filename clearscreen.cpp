3571f24d99ba7673944f6d2a90bdf3153b05e9f5
James Olano <79013899+u3577797@users.noreply.github.com>
2021-04-26 21:43:01 +0800

    Update clearscreen.cpp
    Add comment

--- clearscreen.cpp
+++ clearscreen.cpp
@@ -2,7 +2,7 @@
 #include "clearscreen.h"
 using namespace std;
 
-void clearscreen()
+void clearscreen()                  //this outputs blanks spaces equivalent to the size of the screen
 {
     cout << "\033[2J\033[1;1H";
 }
