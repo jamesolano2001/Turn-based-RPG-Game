a2eec8f03e813a15af351819c8bdcd5d1834d516
3vl01 <78291071+3vl01@users.noreply.github.com>
2021-04-26 21:29:02 +0800

    Update alkaline.cpp

--- alkaline.cpp
+++ alkaline.cpp
@@ -10,19 +10,25 @@ void alkaline_battle(){
     int players_healthbar=player.health;
     int monsters_healthbar=100;
     cout << "This monster in front of you is contaminated by sulphuric acid, so it attacks people with its acidic saliva. The floor has 4 kinds of liquids. Which would you choose to use?"<<endl;
+    //print out the question
     cout << "Your health bar: " << players_healthbar <<endl;
     cout << "Monster's health bar: " << monsters_healthbar <<endl;
+    //print out the health bars
     cout << "/╲/\\╭ºoꍘoº╮/\\╱\\"<< endl;
     cout << "A) A drain cleaner" << endl;
     cout << "B) A bottle of liquid with a pungent smell" << endl;
     cout << "C) A liquid with reddish brown precipitate and rusty smell" << endl;
     cout << "D) A bottle of colorless liquid with vinegar smell" << endl;
     cout << "Q) Quit" << endl;
-    while(players_healthbar>0 && monsters_healthbar>0){
+    //print out the choices and the player can choose to quit anytime
+    //if the player or the monster's health bar reached 0, the battle ends
+    while(players_healthbar>0 && monsters_healthbar>0){     
         cout << "Your health bar: " << players_healthbar <<endl;
         cout << "Monster's health bar: " << monsters_healthbar <<endl;
+	//shows the health bars everytime he/she makes a choice to tell if he/she is correct
         cin >> choice;
         switch(choice){
+	    //responding to his/her answers
             case 'A':
                 monsters_healthbar-=100;
                 break;
@@ -45,11 +51,13 @@ void alkaline_battle(){
                 char ans;
                 cin >> ans;
                 if(ans=='y'){
+		    //if the player chose to quit, he/she will be directed to the city
                     cout<<"You have been revived back to your recent checkpoint"<<endl;
 		    cin.ignore();
 		    alkacity();
                 }
                 else{
+		    //if he/she typed n or other stuff, the battle will continue
                     cout<< "Invalid option" << endl;
                     cin >> choice;
                 }
@@ -58,13 +66,15 @@ void alkaline_battle(){
                 cin >> choice;
         }
     }
-	cin.ignore();
+    cin.ignore();
+    //gives out the correct answer with explaination as this is an educational game
     cout << "A is the correct answer as drain cleaners are made of sodium hydroxide, only NaOH is strong enough to neutralize and burn the monster." << endl;
-	cin.ignore();
+    cin.ignore();
     if(players_healthbar>monsters_healthbar && players_healthbar!=0){
-		clearscreen();
+	clearscreen();
         cout << "You win!!!" <<endl;
 		cin.ignore();
+	        //the health bar will be added to the player.score only when he/she wins in this battle
 		player.score += players_healthbar;
 		cout << "Your score is " << players_healthbar <<endl;
 	   	cin.ignore();
@@ -72,9 +82,11 @@ void alkaline_battle(){
     else{
 	clearscreen();
         cout << "You lose..." <<endl;
+	        //go back to the city to replay this level
 		cout<<"You have been revived back to your recent checkpoint"<<endl;
 		cin.ignore();
 		alkacity();
     }
+    //move forward to the next checkpoint
     return;
 }
