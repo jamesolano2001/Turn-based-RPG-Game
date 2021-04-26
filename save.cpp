ead4afe6c524c531c81991fb2d039ace8564f5df
3vl01 <78291071+3vl01@users.noreply.github.com>
2021-04-26 16:17:37 +0800

    Update save.cpp

--- save.cpp
+++ save.cpp
@@ -6,32 +6,26 @@
 
 using namespace std;
 
-void save(string n, string i, int Score, int Stage, int order, int h, int mh){
-  string filename=n+".txt"
+void save(){
+  string filename=player.name+".txt"
   ofstream fout;
   fout.open(filename);
   if (fout.fail()){
     cout << "Fail saving" << endl;
     exit(1);
   }
-  for (int j=0; j<i.size()-1; j++){
-    fout << i.at(j) << "," ;
+  for (int j=0; j<player.inventory.size()-1; j++){
+    fout << player.inventory.at(j) << "," ;
   }
-  fout << i.at(i.size()) <<endl;
-  fout << Score <<endl;
-  fout << Stage <<endl;
+  fout << player.inventory.at(i.size()) <<endl;
+  fout << player.score <<endl;
+  fout << player.stage <<endl;
   for (int k=0; k<2; k++){
-    fout << order.at(k) << "," ;
+    fout << player.order.at(k) << "," ;
   }
-  fout << order.at(2) << endl;
-  fout << h <<endl;
-  fout << mh <<endl;
+  fout << player.order.at(2) << endl;
+  fout << player.health <<endl;
+  fout << player.maxHealth <<endl;
   fout.close();
 }
 
-int main(){
-  player();
-  Player player;
-  save(player.name, player.inventory, player.score, player.stage, player.order, player.health, player.maxHealth);
-  return 0;
-}
