cc65d8c18c35bc0e35879d960e4f69d7d947ec01
James Olano <79013899+u3577797@users.noreply.github.com>
2021-04-26 14:20:50 +0800

    Update acidville.cpp
    Added in acid battle and fixed bug where if player inputs a string/char instead of a int, it will cause the game to glitch out.

--- acidville.cpp
+++ acidville.cpp
@@ -5,10 +5,11 @@
 #include "acidville.h"
 #include "player.h"
 #include "clearscreen.h"
+#include "acid.h"
 using namespace std;
 
 
-void town()
+void av_town()
 {
     cout<<"  ~         ~~          __         "<<endl;
     cout<<"       _T      .,,.    ~--~ ^^      "<<endl;
@@ -23,26 +24,37 @@ void town()
     cout<<"  ..    ..-''    ;       ''. '       "<<endl;
 }
 
-void menu()
+void av_menu()
 {
-	town();
+	av_town();
 	cout<<"Welcome to Acid Ville"<<endl;
 	cout<<"---------------------"<<endl;
 	cout<<"1. Venture forth into thee forest"<<endl;
 	cout<<"2. Rest"<<endl;
 	cout<<"3. Save"<<endl;
 }
-
+bool avcheckInput(string option)
+{
+	for (int i = 0; i < option.length();i++){
+		if (!isdigit(option[i]))
+			return false;
+	}
+	return true;
+}
 void acidville()
 {
-	int option = 0;
-	srand(time(NULL));
-	cout<<player.name<<endl;
-	while ( option != 1  ){
-		menu();
-		int random = rand()%2;
+	string option;
+	bool rest = true;
+	while (true){
+		av_menu();
 		cin >> option;
-		if ( option == 2 ){
+		while (!avcheckInput(option)){
+			cout<<"Invalid input"<<endl;
+			cin >> option;
+		}
+		int random = rand()%2;
+		if ( option == "2" && rest == true ){
+			cin.ignore();
 			clearscreen();
 			cout<<"zzzZZZ... zzzZZZZZzz... zzzZZZ....."<<endl;
 			cin.ignore();
@@ -50,16 +62,34 @@ void acidville()
 			if ( random == 0){
 				cout<<"Health is now full, that was a good nap!"<<endl;
 				cin.ignore();
+				clearscreen();
 			}
 			else{
 				cout<<"Ouch! The bed bugs have infested the house! What an awful nap!"<<endl;
 				cin.ignore();
+				clearscreen();
 			}
-			option = 0;
+			rest = false;
+		}
+		else if ( option == "2"){
+			cin.ignore();
+			clearscreen();
+			cout<<"No more rests left!"<<endl;
+			cin.ignore();
+			clearscreen();
+		}
+		else if (option == "1" || option == "3"){
+			break;
+		}
+		else{
+			cin.ignore();
+			clearscreen();
+			cout<<"Read the instructions properly!"<<endl;
+			cin.ignore();
+			clearscreen();
 		}
 	}
-
-
-
+	if (option == "1")
+		acid_battle();
 
 }
